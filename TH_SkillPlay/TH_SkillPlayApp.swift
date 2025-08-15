5fbad3100792a209c1171245e16388bb70a2687c
DJUROM777 <djurom777@ya.ru>
2025-08-15 20:02:58 +0300

    Initial Commit

---
+++ TH_SkillPlay/TH_SkillPlayApp.swift
@@ -0,0 +1,17 @@
+//
+//  TH_SkillPlayApp.swift
+//  TH_SkillPlay
+//
+//  Created by IGOR on 15/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct TH_SkillPlayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TH_SkillPlayTests/TH_SkillPlayTests.swift
@@ -0,0 +1,16 @@
+//
+//  TH_SkillPlayTests.swift
+//  TH_SkillPlayTests
+//
+//  Created by IGOR on 15/08/2025.
+//
+
+import Testing
+
+struct TH_SkillPlayTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
